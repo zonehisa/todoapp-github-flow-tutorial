f488eb52e7127e7e30db3eb56e9e50d7230526db
zonehisa <nijipapazoneokun123@gmail.com>
2024-08-26 16:15:57 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ $("#addTodo").click(function(){
   $("input").val("");
 
 });
+
+$(document).on("change", "input[type=checkbox]", function(){
+  if($(this).is(":checked")){
+    $(this).parent().css("text-decoration", "line-through");
+  } else {
+    $(this).parent().css("text-decoration", "none");
+  }
+
+});
